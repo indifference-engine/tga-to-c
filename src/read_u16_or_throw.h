af84c4f4fd06f9d44bfc9adc94cb1984190070f6
siliconspecter <siliconspecter@outlook.com>
2024-05-23 21:41:08 +0100

    Correct comment.

--- src/read_u16_or_throw.h
+++ src/read_u16_or_throw.h
@@ -4,7 +4,7 @@
  * @param description A description of what is being read
  *                    (e.g. "a sub-heading").
  * @param file The file from which to read an unsigned short.
- * @return The unsigned byte read from the file.
+ * @return The unsigned short read from the file.
  */
 uint16_t read_u16_or_throw(
     const char *const description,
